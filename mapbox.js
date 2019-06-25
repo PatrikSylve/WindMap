2926019322007d7b1eef46dcc2c155c9cf69df26
PatrikSylve <lan15psy@student.lu.se>
2019-06-25 18:47:48 +0100

    webgl

--- mapbox.js
+++ mapbox.js
@@ -13,7 +13,57 @@ var bbox = new mapboxgl.LngLatBounds(
       zoom: 2
   });
   
+
+  
   // set darkmode
   map.setStyle('mapbox://styles/mapbox/dark-v10');
   // set maximum bounding box to only cover sweden 
   map.setMaxBounds(bbox); 
+
+  var highlightLayer = {
+    id: 'highlight',
+    type: 'custom',
+     
+    onAdd: function (map, gl) {
+    var vsSource = "" +
+    "uniform mat4 u_matrix;" +
+    "attribute vec2 a_pos;" +
+    "void main() {" +
+    "    gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);" +
+    "}";
+     
+    var fsSource = "" +
+    "void main() {" +
+    "    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);" +
+    "}";
+
+    this.program = createShaderProgram(gl, fsSource, vsSource);
+
+    this.aPos = gl.getAttribLocation(this.program, "a_pos");
+     
+    var helsinki = mapboxgl.MercatorCoordinate.fromLngLat({ lng: 25.004, lat: 60.239 });
+    var berlin = mapboxgl.MercatorCoordinate.fromLngLat({ lng: 13.403, lat: 52.562 });
+    var kyiv = mapboxgl.MercatorCoordinate.fromLngLat({ lng: 30.498, lat: 50.541 });
+     
+    this.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    helsinki.x, helsinki.y,
+    berlin.x, berlin.y,
+    kyiv.x, kyiv.y,
+    ]), gl.STATIC_DRAW); 
+  },
+  render: function (gl, matrix) {
+    gl.useProgram(this.program);
+    gl.uniformMatrix4fv(gl.getUniformLocation(this.program, "u_matrix"), false, matrix);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.enableVertexAttribArray(this.aPos);
+    gl.vertexAttribPointer(this.aPos, 2, gl.FLOAT, false, 0, 0);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+    }
+  };
+  map.on('load', function() {
+map.addLayer(highlightLayer, 'building');
+});
