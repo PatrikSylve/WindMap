a2abbc5d0024055311a4ec787ecba9b109fa703d
PatrikSylve <lan15psy@student.lu.se>
2019-07-12 12:00:36 +0100

    texture working

--- mapbox.js
+++ mapbox.js
@@ -1,18 +1,17 @@
 
 var imgCoord = new Image();
 //img.src="coordRandom.png";
-imgCoord.src="coordRandom.png";
+imgCoord.src="textures/coordRandom.png";
 var imgCoord2 = new Image();
 //img.src="coordRandom.png";
-imgCoord2.src="coordRandom.png";
+imgCoord2.src="textures/coordRandom.png";
 var imgWind = new Image();
-imgWind.src = "windRandom.png"; 
+imgWind.src = "textures/windRandom.png"; 
 imgCoord.onload = function () {
   imgWind.onload = function() {
   initMap([imgCoord,imgWind, imgCoord2])
   }
 }
-
 
 function initMap(textures){
 
@@ -39,6 +38,8 @@ function initMap(textures){
       // add custom layer from class PointLayer
         map.addLayer(new PointLayer(textures));
     });
+   
+
 }
 
 
@@ -54,7 +55,8 @@ class PointLayer {
       this.id = 'point-layer';
       this.type = 'custom';
       this.renderingMode = '2d';
-      this.textures = textures; 
+      this.textures = textures;  
+      this.particleRes = 512;
   }
 
   onAdd(map, gl) {
@@ -65,8 +67,7 @@ class PointLayer {
       attribute float a_index;
 
       uniform sampler2D u_particles;
-      uniform sampler2D u_wind; 
-  
+      uniform sampler2D u_wind;   
       uniform float u_particles_res;
       uniform mat4 u_matrix;
 
@@ -97,44 +98,39 @@ class PointLayer {
         floor(a_index / u_particles_res) / u_particles_res));
       v_color = w_tex;
 
+      // get velocity 
+      vec2 velocity = vec2(v_color.xy/255.0);
     
         
-      // decode coordtexture pixels
+       // decode coordtexture pixels
         float decoded_x = decode(color.xy, 18.0); 
         float decoded_y = decode(color.zw, 18.0); 
 
-      //convert coordinates to mercator range [0-1]
+        //convert coordinates to mercator range [0-1]
         vec2 projected_pos = project(vec2(decoded_x,decoded_y));
 
-   
-
         gl_PointSize = 2.0;
-        gl_Position = u_matrix*vec4(projected_pos, 0, 1);
+        gl_Position = u_matrix*vec4(projected_pos+velocity, 0, 1);
     }`;
 
-      const fragmentSource = `
-      precision mediump float;
-      varying vec4 v_color;
-      void main() { 
-          gl_FragColor = v_color;//vec4(1.0, 0.0, 0.0, 1.0);
-      }`;
+    const fragmentSource = `
+    precision mediump float;
+    
+    
+    varying vec4 v_color;
+    void main() { 
+ 
+    gl_FragColor = v_color;//vec4(1.0, 0.0, 0.0, 1.0);
+    }`;
 
       var indexCoord = []; 
       for (var i = 0; i < 512*512; i++){
         indexCoord[i] = i; 
       }
 
-      const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-      gl.shaderSource(vertexShader, vertexSource);
-      gl.compileShader(vertexShader);
-      const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-      gl.shaderSource(fragmentShader, fragmentSource);
-      gl.compileShader(fragmentShader);
-
-      this.program = gl.createProgram();
-      gl.attachShader(this.program, vertexShader);
-      gl.attachShader(this.program, fragmentShader);
-      gl.linkProgram(this.program);
+      // init shaders 
+      this.program = initShaders(gl, vertexSource, fragmentSource); 
+  
 
       this.aPos = gl.getAttribLocation(this.program, "a_index");
       this.buffer = gl.createBuffer();
@@ -143,40 +139,18 @@ class PointLayer {
 
       this.textures = genTexture(gl, this.textures);
 
-      /*
-          // Create a texture.
-    var texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-     
-    // Fill the texture with a 1x1 blue pixel.
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-                  new Uint8Array([0, 0, 255, 255]));
-     
-    // Asynchronously load an image
-    var image2 = new Image();
-    image2.src = "projectedCoord.png";
-    image2.addEventListener('load', function() {
-      // Now that the image has loaded make copy it to the texture.
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      console.log("hj")
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image2);
-      gl.generateMipmap(gl.TEXTURE_2D);
-    });
-    */
-   gl.useProgram(this.program);
-   var u_image0Location = gl.getUniformLocation(this.program, "u_particles");
-   var u_image1Location = gl.getUniformLocation(this.program, "u_wind");
 
-   gl.uniform1i(u_image0Location, 0);  // texture unit 0
-   gl.uniform1i(u_image1Location, 1);  // texture unit 1
+      gl.useProgram(this.program);
+      var u_image0Location = gl.getUniformLocation(this.program, "u_particles");
+      var u_image1Location = gl.getUniformLocation(this.program, "u_wind");
 
- 
+      gl.uniform1i(u_image0Location, 0);  // texture unit 0
+      gl.uniform1i(u_image1Location, 1);  // texture unit 1
 
   }
 
   render(gl, matrix) {
-      var particleRes = 512; 
-     
+
       gl.activeTexture(gl.TEXTURE0);
       gl.bindTexture(gl.TEXTURE_2D, this.textures[0]);
       gl.activeTexture(gl.TEXTURE1);
@@ -186,16 +160,19 @@ class PointLayer {
       gl.enableVertexAttribArray(this.aPos);
       gl.vertexAttribPointer(this.aPos, 1, gl.FLOAT, false, 0, 0);
       gl.useProgram(this.program);
-      gl.uniform1f(gl.getUniformLocation(this.program, "u_particles_res"), particleRes);
+      gl.uniform1f(gl.getUniformLocation(this.program, "u_particles_res"), this.particleRes);
+      gl.uniform1f(gl.getUniformLocation(this.program, "u_velocity"), this.velocity);
+
       gl.uniformMatrix4fv(gl.getUniformLocation(this.program, "u_matrix"), false, matrix);
       gl.drawArrays(gl.POINTS, 0, 512*512); // nbr of points
   }
 }
 
 
+
 function genTexture(gl, image){
   var textures = []
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 2; i++) {
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
@@ -211,14 +188,35 @@ function genTexture(gl, image){
   return textures; 
 }
 
+function initShaders(gl, vertexSource, fragmentSource){
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexSource);
+  gl.compileShader(vertexShader);
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentSource);
+  gl.compileShader(fragmentShader);
+
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  return program; 
+}
+
+ 
+function bindFramebuffer(gl, framebuffer, texture) {
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+  if (texture) {
+      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+  }
+}
+
+/*
 class Particles {
   constructor(numParticles, gl){
     this.numParticles =  numParticles; 
     this.gl = gl;
   }
-
-  
-
 }
- 
 
+*/
