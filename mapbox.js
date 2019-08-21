67b5e10873aa1acb5d325ccb0d758f058dc6f6d9
PatrikSylve <lan15psy@student.lu.se>
2019-08-21 11:51:52 +0100

    minor error fix

--- mapbox.js
+++ mapbox.js
@@ -126,8 +126,7 @@ class PointLayer {
           vec4 color = texture2D(u_particles, vec2(fract(a_index / u_particles_res),
                                   floor(a_index / u_particles_res) / u_particles_res));
   
-          vec2 particle_pos = vec2(color.r / 255.0 + color.b,
-                                  color.g / 255.0 + color.a);
+          vec2 particle_pos = vec2(color.r / 255.0 + color.b, color.g / 255.0 + color.a);
   
           v_pos = particle_pos; 
   
